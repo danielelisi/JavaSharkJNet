bcb7d025007311c85cfb4b0b029de5a1dc8a0c28
Daniele Lisi <danielelisi978@gmail.com>
2017-03-29 18:06:02 -0700

    implementing file loop

--- src/Examples/api.java
+++ src/Examples/api.java
@@ -1,7 +1,32 @@
 package Examples;
 
+import org.jnetpcap.Pcap;
+import org.jnetpcap.packet.JPacketHandler;
+import org.jnetpcap.protocol.tcpip.Http;
+import org.jnetpcap.protocol.tcpip.Tcp;
+
 /**
  * Created by danielelisi on 2017-03-29.
  */
 public class api {
+
+    public static void main(String[] args){
+
+        final String WFILE = "wireshark.pcap";
+        final StringBuilder ERROR_BUFFER = new StringBuilder();
+
+        final Pcap pcapFile = Pcap.openOffline(WFILE, ERROR_BUFFER);
+
+        // Read first 10 packets from wireshark file
+        pcapFile.loop(10, new JPacketHandler<StringBuilder>() {
+
+            final Tcp TCP = new Tcp();
+            final Http HTTP = new Http();
+
+
+
+
+        });
+
+    }
 }
