685a7fbf595657de256c192d859d6a121457fda8
Daniele Lisi <danielelisi978@gmail.com>
2017-04-02 16:48:15 -0700

    selectFile button implemented

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -1,4 +1,35 @@
 package Application;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    private Button selectButton;
+
+    @FXML
+    private Button loadButton;
+
+    @FXML
+    private TextField filePath;
+
+    public void chooseFile (ActionEvent event) {
+        FileChooser fc = new FileChooser();
+
+        File selectedFile = fc.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            filePath.setText(selectedFile.getAbsolutePath());
+        } else {
+            System.out.println("File is not selected.");
+        }
+    }
+
 }
